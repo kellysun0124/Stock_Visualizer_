d54f8a803d636171d6ca9fb7115474d99747c714
KellySun <kellysun0124@gmail.com>
2023-10-23 14:51:50 -0500

    add comments of app requirements for project

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from script import fetch_all_stock_data 
 
-#ask for stock symbol and return data
+#Ask the user to enter the stock symbol for the company they want data for.
+#and return data for said company
 def getSymbol():
     stock_symbol = input("enter the stock symbol for the company:  ")
     #gets data from API
@@ -17,3 +18,10 @@ def main():
 
 
 main()
+
+#Ask the user for the chart type they would like.
+#Ask the user for the time series function they want the api to use.
+#Ask the user for the beginning date in YYYY-MM-DD format.
+#Ask the user for the end date in YYYY-MM-DD format.
+#The end date should not be before the begin date
+#Generate a graph and open in the user’s default browser.
