581ce0f4034a09d47fd53c9b0a2e798a5b9a5196
Cade Holtzen <choltzenjr@gmail.com>
2023-10-24 17:56:35 -0500

    Cleaned up time series and main functions

--- main.py
+++ main.py
@@ -8,22 +8,7 @@ def getSymbol():
     return fetch_all_stock_data(stock_symbol)
 
 # Define a function that takes the time series number as an argument and returns the corresponding function name
-def get_time_series_function(time_series):
-    # Use a dictionary to map the numbers to the function names
-    time_series_dict = {
-        "1": "TIME_SERIES_INTRADAY",
-        "2": "TIME_SERIES_DAILY",
-        "3": "TIME_SERIES_WEEKLY",
-        "4": "TIME_SERIES_MONTHLY"
-    }
-    # Return the function name if the number is valid, otherwise return None
-    return time_series_dict.get(time_series, None)
-
-def main():
-    print("Stock Data Visualizer")
-    print("------------------------")
-    getSymbol()
-    
+def get_time_series_function():
     print("Enter the time series function:")
     print("1. TIME_SERIES_INTRADAY")
     print("2. TIME_SERIES_DAILY")
@@ -31,9 +16,30 @@ def main():
     print("4. TIME_SERIES_MONTHLY")
     time_series = input("Enter the number: ")
 
+    time_series_dict = {
+        "1": "TIME_SERIES_INTRADAY",
+        "2": "TIME_SERIES_DAILY",
+        "3": "TIME_SERIES_WEEKLY",
+        "4": "TIME_SERIES_MONTHLY"
+    }
 
+    # Return the function name if the number is valid, otherwise return None
+    return time_series_dict.get(time_series, None)
 
-main()
+def main():
+    print("Stock Data Visualizer")
+    print("------------------------")
+    getSymbol()
+
+    time_series = get_time_series_function()
+    if time_series is not None:
+        print(f"Selected time series function: {time_series}")
+    else:
+        print("Invalid selection.")
+
+if __name__ == "__main__":
+    main()
+
 
 #Ask the user for the chart type they would like.
 #Ask the user for the time series function they want the api to use.
